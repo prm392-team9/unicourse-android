3aad844fcd8ffe511547a8ba4595238ba1dbc1ed
nguyenhuykhai <nguyenhuykhaipch94@gmail.com>
2024-05-31 20:06:43 +0700

    🎨 [style]: Add carosel for landing page

---
+++ app/src/main/java/com/example/unicourse/ui/activities/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.unicourse.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.unicourse.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+    }
+}
