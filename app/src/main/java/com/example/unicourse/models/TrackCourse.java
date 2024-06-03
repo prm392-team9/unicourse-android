b29ab15b694bad3e5c83d48dd51416d496e85488
Nguyễn Huy Khải <66985115+nguyenhuykhai@users.noreply.github.com>
2024-06-03 21:02:05 +0700

    💄 [style]: Update UI for landing page

---
+++ app/src/main/java/com/example/unicourse/models/TrackCourse.java
@@ -0,0 +1,50 @@
+package com.example.unicourse.models;
+
+// TrackCourse and SubTrack classes should also be defined accordingly
+public class TrackCourse {
+    private String chapterTitle;
+    private SubTrack track_steps;
+    private Number position;
+
+    public TrackCourse() {
+    }
+
+    public TrackCourse(String chapterTitle, SubTrack track_steps, Number position) {
+        this.chapterTitle = chapterTitle;
+        this.track_steps = track_steps;
+        this.position = position;
+    }
+
+    public String getChapterTitle() {
+        return chapterTitle;
+    }
+
+    public void setChapterTitle(String chapterTitle) {
+        this.chapterTitle = chapterTitle;
+    }
+
+    public SubTrack getTrack_steps() {
+        return track_steps;
+    }
+
+    public void setTrack_steps(SubTrack track_steps) {
+        this.track_steps = track_steps;
+    }
+
+    public Number getPosition() {
+        return position;
+    }
+
+    public void setPosition(Number position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "TrackCourse{" +
+                "chapterTitle='" + chapterTitle + '\'' +
+                ", track_steps=" + track_steps +
+                ", position=" + position +
+                '}';
+    }
+}
