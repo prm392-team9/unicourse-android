1a6dca57edbbe46e2f52870b7fcb9d34356ee973
Nguyễn Trung Kiên <trungkiennguyen0310@gmail.com>
2024-06-12 11:43:28 +0700

    format getter, setter code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
--- app/src/main/java/com/example/unicourse/models/TrackCourse.java
+++ app/src/main/java/com/example/unicourse/models/TrackCourse.java
@@ -2,44 +2,23 @@ package com.example.unicourse.models;
 
 import java.util.ArrayList;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 // TrackCourse and SubTrack classes should also be defined accordingly
 public class TrackCourse {
     private String chapterTitle;
     private ArrayList<SubTrack> track_steps;
     private Number position;
 
-    public TrackCourse() {
-    }
 
-    public TrackCourse(String chapterTitle, ArrayList<SubTrack> track_steps, Number position) {
-        this.chapterTitle = chapterTitle;
-        this.track_steps = track_steps;
-        this.position = position;
-    }
 
-    public String getChapterTitle() {
-        return chapterTitle;
-    }
-
-    public void setChapterTitle(String chapterTitle) {
-        this.chapterTitle = chapterTitle;
-    }
-
-    public ArrayList<SubTrack> getTrack_steps() {
-        return track_steps;
-    }
-
-    public void setTrack_steps(ArrayList<SubTrack> track_steps) {
-        this.track_steps = track_steps;
-    }
-
-    public Number getPosition() {
-        return position;
-    }
-
-    public void setPosition(Number position) {
-        this.position = position;
-    }
 
     @Override
     public String toString() {
