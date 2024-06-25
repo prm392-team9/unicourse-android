d930829e909269f886e45bda0ae547ab3d5b04e6
Hoang Anh <hoanganh.luong2906@gmail.com>
2024-06-25 09:20:08 +0700

    Course Adapter

---
+++ app/src/main/java/com/example/unicourse/adapters/ProfileAdapter.java
@@ -0,0 +1,65 @@
+package com.example.unicourse.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.unicourse.R;
+import com.example.unicourse.models.ProfileCourse;
+
+import java.util.ArrayList;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.CardViewHolder> {
+
+    private final Context pContext;
+    private final ArrayList<ProfileCourse> profileCourses;
+
+    public ProfileAdapter(Context pContext, ArrayList<ProfileCourse> profileCourses) {
+        this.pContext = pContext;
+        this.profileCourses = profileCourses;
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(pContext);
+        View view = inflater.inflate(R.layout.activity_profile_recent_card_item, parent, false);
+        return new CardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        ProfileCourse profileCourse = profileCourses.get(position);
+        holder.bind(profileCourse);
+    }
+
+    @Override
+    public int getItemCount() {
+        return profileCourses.size();
+    }
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView courseTitle;
+
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.recentCourseImage);
+            courseTitle = itemView.findViewById(R.id.recentCourseTitle);
+        }
+
+        public void bind(ProfileCourse profileCourse) {
+            Glide.with(imageView.getContext())
+                    .load(profileCourse.getImage())
+                    .into(imageView);
+            courseTitle.setText(profileCourse.getTitle());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/unicourse/models/ProfileCourse.java
@@ -0,0 +1,29 @@
+package com.example.unicourse.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileCourse {
+    private String title;
+    private String image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
