07a0b70e5c8c83c43891e89a4939921b1b2c40d0
Trung Kiên Nguyễn <trungkiennguyen0310@gmail.com>
2024-07-12 13:15:06 +0700

    config google map

--- app/src/main/java/com/example/unicourse/ui/fragments/ContactFragment.java
+++ app/src/main/java/com/example/unicourse/ui/fragments/ContactFragment.java
@@ -1,9 +1,11 @@
 package com.example.unicourse.ui.fragments;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ContactFragment extends Fragment implements OnMapReadyCallback {
 
+    private static final String TAG = "ContactFragment";
     private GoogleMap mMap;
     private FragmentContactBinding binding;
 
@@ -41,6 +45,20 @@ public class ContactFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
 
         // Add a marker in FPT University and move the camera
         LatLng fptUniversity = new LatLng(10.8411276, 106.809883);
